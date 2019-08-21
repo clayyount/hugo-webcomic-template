d28cbf5047bee149e419b088336e4ae82767623b
Yount, Clay <Clay.Yount@ellucian.com>
2019-08-21 17:41:46 -0400

    *

--- node_modules/netlify-cms/node_modules/netlify-cms-app/dist/esm/editor-components.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _netlifyCmsCore = require("netlify-cms-core");
-
-var _netlifyCmsEditorComponentImage = _interopRequireDefault(require("netlify-cms-editor-component-image"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_netlifyCmsCore.NetlifyCmsCore.registerEditorComponent(_netlifyCmsEditorComponentImage.default);
